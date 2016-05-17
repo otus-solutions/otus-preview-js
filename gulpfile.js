4c0f10774eeb1bc2b1248bda6b407dbc786b85fa
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2016-05-17 15:19:29 -0300

    Construção Basica Projeto

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+(function() {
+
+    var gulp = require('gulp');
+    var bump = require('gulp-bump');
+    var uglify = require("gulp-uglify");
+    var minify = require('gulp-minify');
+    var concat = require('gulp-concat');
+
+    gulp.task('upgrade-version', function(value) {
+        gulp.src('./package.json')
+            .pipe(bump({
+                version: process.env.npm_config_value
+            }))
+            .pipe(gulp.dest('./'));
+    });
+
+    gulp.task('compress', function() {
+        gulp.src(['app/**/*-module.js', 'app/**/*.js'])
+            .pipe(concat('otus-preview.js'))
+            .pipe(minify({'mangle' : false}))
+            .pipe(gulp.dest('dist'));
+    });
+
+}());
+
