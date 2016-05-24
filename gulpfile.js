d2f86cffa3c7e74ca9367907eccddb81f8087f58
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2016-05-24 15:02:05 -0300

    Integração JS e Sonar

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@
     var uglify = require("gulp-uglify");
     var minify = require('gulp-minify');
     var concat = require('gulp-concat');
+    var sonar = require('gulp-sonar');
+    var packageJson = require('./package.json');
 
     gulp.task('upgrade-version', function(value) {
         gulp.src('./package.json')
@@ -17,9 +19,41 @@
     gulp.task('compress', function() {
         gulp.src(['app/**/*-module.js', 'app/**/*.js'])
             .pipe(concat('otus-preview.js'))
-            .pipe(minify({'mangle' : false}))
+            .pipe(minify({
+                'mangle': false
+            }))
             .pipe(gulp.dest('dist'));
     });
 
-}());
+    gulp.task('sonar', function() {
+        var options = {
+            sonar: {
+                host: {
+                    url: process.env.npm_config_sonarUrl,
+                },
+                jdbc: {
+                    url: process.env.npm_config_sonarDatabaseUrl,
+                    username: process.env.npm_config_sonarDatabaseUsername,
+                    password: process.env.npm_config_sonarDatabasePassword
+                },
+                projectKey: 'sonar:otus-preview-js',
+                projectName: 'otus-preview-js',
+                projectVersion: packageJson.version,
+                // comma-delimited string of source directories
+                sources: 'app',
+                language: 'js',
+                sourceEncoding: 'UTF-8',
+                exec: {
+                    maxBuffer: 1024 * 1024
+                }
+            }
+        };
 
+        return gulp.src('thisFileDoesNotExist.js', {
+                read: false
+            })
+            .pipe(sonar(options));
+    });
+
+
+}());
