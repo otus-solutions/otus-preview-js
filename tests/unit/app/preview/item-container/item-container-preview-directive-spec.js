fb9cfc778b08e0ab77349e8c0b9f5e09c3c5fe68
Emanoel Vianna <emanoel.vianna@outlook.com>
2016-05-25 16:42:07 -0300

    FOR-60 - começo do test de unidade

---
+++ tests/unit/app/preview/item-container/item-container-preview-directive-spec.js
@@ -0,0 +1,22 @@
+describe('otusSheetPreview', function() {
+    var Mock = {};
+
+    beforeEach(module('otus.preview'));
+
+    beforeEach(inject(function(_$rootScope_, _$compile_) {
+        $rootScope = _$rootScope_;
+        $compile = _$compile_;
+    }));
+
+    describe('test of directive otusSheetPreview', function() {
+        it('element must be well defined', function() {
+            var question = $compile("<question-preview></question-preview>")($rootScope);
+            var text = $compile("<text-item-preview></text-item-preview>")($rootScope);
+            var image = $compile("<image-item-preview></image-item-preview>")($rootScope);
+
+            expect(question).toBeDefined();
+            expect(text).toBeDefined();
+            expect(image).toBeDefined();
+        });
+    });
+});
---
+++ tests/unit/app/preview/sheet/sheet-preview-directive-spec.js
@@ -0,0 +1,31 @@
+describe('otusSheetPreview', function() {
+
+    var $route, $rootScope, $location, $httpBackend;
+
+    beforeEach(module('otus.preview'));
+
+    beforeEach(inject(function($injector) {
+        //$route = $injector.get('$route');
+        $rootScope = $injector.get('$rootScope');
+        $location = $injector.get('$location');
+        $httpBackend = $injector.get('$httpBackend');
+
+        $httpBackend.when('GET', 'sheet-preview-container.html').respond('sheet');
+    }));
+
+    describe('test of directive otusSheetPreview', function() {
+        it('element must be well defined', function() {
+            var element = $compile("<otus-sheet-preview></otus-sheet-preview>")($rootScope);
+            expect(element).toBeDefined();
+        });
+
+        xit('should navigate to page sheet-preview-container', function() {
+
+            $rootScope.$apply(function() {
+                $location.path('/sheet');
+            });
+            expect($location.path()).toBe('/sheet');
+            expect($route.current.templateUrl).toBe('sheet-preview-container.html');
+        });
+    });
+});
