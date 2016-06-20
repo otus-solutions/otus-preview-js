ec02b61dba2bca2ab477ef11c18f09f4a26fb04f
Emanoel Vianna <emanoel.vianna@outlook.com>
2016-06-20 14:43:53 -0300

    FOR-60 modificação no nome do serviço

--- app/ui-preview/sheet/sheet-preview-directive.js
+++ app/ui-preview/sheet/sheet-preview-directive.js
@@ -10,11 +10,6 @@
             scope: {},
             templateUrl: 'node_modules/otus-preview-js/app/ui-preview/sheet/sheet-preview-container.html',
             retrict: 'E',
-            link: function (scope) {
-                scope.$on("$destroy", function () {
-                    console.log("directive destroy");
-                });
-            }
         };
         return ddo;
     }
