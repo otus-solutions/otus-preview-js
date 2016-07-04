86518af0354d79df2fad449e38730698270c5bbc
fagner <fagnerschwalm@gmail.com>
2016-07-04 17:53:53 -0300

    FOR-60 - Criado serviços para montar as tags html dinâmicamente.

---
+++ app/service/html-builder-service.js
@@ -0,0 +1,36 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('otus.preview.component')
+        .service('HtmlBuilderService', HtmlBuilderService);
+
+    function HtmlBuilderService() {
+        var self = this;
+
+        self.generateTagName = generateTagName;
+
+        function generateTagName(stringToFormat) {
+            var chars = stringToFormat.split('');
+            var tagName = '';
+
+            chars.forEach(function(character, index) {
+                var lowerChar = '';
+
+                if (character === character.toUpperCase()) {
+                    lowerChar = character.toLowerCase();
+                    if (index !== 0) {
+                        lowerChar = '-' + lowerChar;
+                    }
+                } else {
+                    lowerChar = character;
+                }
+
+                tagName = tagName + lowerChar;
+            });
+
+            return tagName;
+        }
+    }
+
+})();
---
+++ app/service/tag-component-builder-service.js
@@ -0,0 +1,24 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('otus.preview.component')
+        .service('TagComponentBuilderService', TagComponentBuilderService);
+
+    TagComponentBuilderService.$inject = ['HtmlBuilderService'];
+
+    function TagComponentBuilderService(HtmlBuilderService) {
+        var self = this;
+
+        self.createTagElement = createTagElement;
+
+        function createTagElement(elementType) {
+            return _replace(HtmlBuilderService.generateTagName(elementType));
+        }
+
+        function _replace(tagName){
+            return '<' + tagName  + ' item-data="$ctrl.itemData" />';
+        }
+    }
+
+})();
