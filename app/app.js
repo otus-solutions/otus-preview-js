3597d1d744f132bcdd2b86faab3fb06bee4b0074
Diego Santos <santos.diegoferreira@gmail.com>
2016-07-14 13:26:47 -0300

    App sem dependências.

--- app/app.js
+++ app/app.js
@@ -3,13 +3,6 @@
 
     angular
         .module('otus.preview', [
-            /* External modules */
-            'ui.router',
-            'ngMaterial',
-            'angular-bind-html-compile',
-            /* Otus model module */
-            'otusjs',
-            /* Components modules */
             'otus.component.allPurpose',
             'otus.component.editor',
             'otus.component.preview'
