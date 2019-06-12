0dc8e90565754f03b0c0b3ccaad293256a4ba54f
Fabiano Ramires <fdrtec@gmail.com>
2019-06-12 12:43:01 -0300

    OPJ-39 ajuste no karma para traduzir let no ecmaScript

---
+++ tests/unit/app/otusjs-player-data/viewer/survey-viewer-model-factory-spec.js
@@ -0,0 +1,11 @@
+describe('surveyViewerModelFactory_TestSuite', function () {
+  var factory;
+  var Injections = [];
+  var Mock = {};
+
+  beforeEach(function () {
+    angular.mock.module(otusjs.player.standalone);
+
+  })
+
+});
