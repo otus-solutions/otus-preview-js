40f287eba21a12bb183d656a847c6b142cca9590
Adriano <adriano@otus-solutions.com.br>
2019-12-12 13:00:16 -0300

    POE-91 adição de inputmode

--- dist/otus-preview-js/scripts/otusjs-player-min.js
+++ dist/otus-preview-js/scripts/otusjs-player-min.js
@@ -1685,7 +1685,7 @@
   angular
     .module('otusjs.player.component')
     .component('otusDecimalQuestion', {
-      template:'<md-content layout-padding><div layout="row"><md-input-container md-no-float class="md-block" flex-gt-sm="45"><input type="number" step="any" ng-model="$ctrl.answer" ng-change="$ctrl.update()" ng-if="!$ctrl.mobileInput" ui-decimal placeholder="Insira um valor decimal" ng-disabled="$ctrl.view"> <input type="number" step="any" ng-model="$ctrl.answer" ng-change="$ctrl.update()" ng-if="$ctrl.mobileInput" placeholder="Insira um valor decimal" ng-disabled="$ctrl.view"></md-input-container><md-input-container class="md-block" flex-gt-sm="45"><otus-label item-label="$ctrl.itemData.unit"></otus-label></md-input-container></div></md-content>',
+      template:'<md-content layout-padding><div layout="row"><md-input-container md-no-float class="md-block" flex-gt-sm="45"><input type="number" step="any" ng-model="$ctrl.answer" ng-change="$ctrl.update()" ng-if="!$ctrl.mobileInput" ui-decimal placeholder="Insira um valor decimal" ng-disabled="$ctrl.view"> <input type="number" inputmode="decimal" step="any" ng-model="$ctrl.answer" ng-change="$ctrl.update()" ng-if="$ctrl.mobileInput" placeholder="Insira um valor decimal" ng-disabled="$ctrl.view"></md-input-container><md-input-container class="md-block" flex-gt-sm="45"><otus-label item-label="$ctrl.itemData.unit"></otus-label></md-input-container></div></md-content>',
       controller: "otusDecimalQuestionCtrl as $ctrl",
       bindings: {
         itemData: '<',
@@ -1740,7 +1740,7 @@
   angular
     .module('otusjs.player.component')
     .component('otusDecimalQuestionView', {
-      template:'<md-content layout-padding><div layout="row"><md-input-container md-no-float class="md-block" flex-gt-sm="45"><input type="number" step="any" ng-model="$ctrl.answer" ng-change="$ctrl.update()" ng-if="!$ctrl.mobileInput" ui-decimal placeholder="Insira um valor decimal" ng-disabled="$ctrl.view"> <input type="number" step="any" ng-model="$ctrl.answer" ng-change="$ctrl.update()" ng-if="$ctrl.mobileInput" placeholder="Insira um valor decimal" ng-disabled="$ctrl.view"></md-input-container><md-input-container class="md-block" flex-gt-sm="45"><otus-label item-label="$ctrl.itemData.unit"></otus-label></md-input-container></div></md-content>',
+      template:'<md-content layout-padding><div layout="row"><md-input-container md-no-float class="md-block" flex-gt-sm="45"><input type="number" step="any" ng-model="$ctrl.answer" ng-change="$ctrl.update()" ng-if="!$ctrl.mobileInput" ui-decimal placeholder="Insira um valor decimal" ng-disabled="$ctrl.view"> <input type="number" inputmode="decimal" step="any" ng-model="$ctrl.answer" ng-change="$ctrl.update()" ng-if="$ctrl.mobileInput" placeholder="Insira um valor decimal" ng-disabled="$ctrl.view"></md-input-container><md-input-container class="md-block" flex-gt-sm="45"><otus-label item-label="$ctrl.itemData.unit"></otus-label></md-input-container></div></md-content>',
       controller: "otusDecimalQuestionViewCtrl as $ctrl",
       bindings: {
         itemData: '<'
